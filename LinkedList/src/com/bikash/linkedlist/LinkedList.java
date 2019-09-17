d338217d2a2fdbec73d7ba4e5669f82c3f467262
Bikash Kumar <bikas.kumar@sourcebits.com>
2019-09-17 09:29:47 +0530

    Display and Add first function added

--- LinkedList/src/com/bikash/linkedlist/LinkedList.java
+++ LinkedList/src/com/bikash/linkedlist/LinkedList.java
@@ -1,5 +1,11 @@
 package com.bikash.linkedlist;
 
+/**
+ * Custom LinkedList with all possible operations 
+ * 
+ * @author Bikash Kumar
+ *
+ */
 public class LinkedList {
 	int size;
 	Node start;
@@ -9,24 +15,68 @@ public class LinkedList {
 		start = null;
 	}
 
+	/**
+	 * Display all Node's data 
+	 * Take one pointer node to traverse the whole List
+	 */
 	public void viewList() {
-
+		Node node = null;
+		if (isEmpty()) {
+			System.out.println("List is Empty.");
+		} else {
+			// initialize the temporary node to start node
+			node = start;
+			// print from first element to last element of the List
+			for (int i = 0; i < size; i++) {
+				System.out.print(node.getData()+"->");
+				// get next node address
+				node = node.getNext();
+			}
+		}
 	}
 
+	/**
+	 * If start node is null, then return true
+	 * @return boolean
+	 */
 	public boolean isEmpty() {
-		return false;
+		if (start == null)
+			return true;
+		else
+			return false;
 	}
 
+	/**
+	 * Total size of the List
+	 * @return boolean
+	 */
 	public int getListSize() {
 		return size;
 	}
 
 	public void insertAtFirst(int value) {
-
+		Node node = new Node();
+		node.setData(value);
+		// set current start address
+		node.setNext(start);
+		start = node;
+		size++;
 	}
 
 	public void insertAtLast(int value) {
-
+		Node node = null;
+		if (isEmpty()) {
+			System.out.println("List is Empty.");
+		} else {
+			// initialize the temporary node to start node
+			node = start;
+			// print from first element to last element of the List
+			for (int i = 0; i < size; i++) {
+				// get next node address
+				node = node.getNext();
+			}
+			
+		}
 	}
 
 	public void insertAtPosition(int value, int position) {
