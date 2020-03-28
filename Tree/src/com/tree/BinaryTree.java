2ac745b0121502f099e427816b4242b816f95d46
Bikash Kumar <bikas.kumar@sourcebits.com>
2020-03-28 11:43:49 +0530

    Tree Project Included
    Tree Project Included

---
+++ Tree/src/com/tree/BinaryTree.java
@@ -0,0 +1,28 @@
+package com.tree;
+
+import java.util.Arrays;
+
+public class BinaryTree {
+	public static void main(String[] args) {
+		// int sum = 0;
+		// for (int i = 0, j = 0; i < 5 & j < 5; ++i, j = i + 1)
+		// sum += i;
+		// System.out.println(sum);
+
+//		int[] a = { 34, 3, 2, 67, 89, 4, 3 };
+//		for (int i = 0; i < a.length - 1; i = i + 2) {
+//			if ((a[i] > a[i + 1])) {
+//				int temp = a[i];
+//				a[i] = a[i + 1];
+//				a[i + 1] = temp;
+//			}
+//		}
+//		System.out.println(Arrays.toString(a));
+		
+		    System.out.println(fun()); 
+	}
+	static int fun() { 
+		return 20; 
+	}  
+
+}
